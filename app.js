ed7d95ed067da3e1985daad562e268f51ffc026f
AnAnh2k <anducanh125@gmail.com>
2025-09-23 20:55:07 +0700

    video 24

--- app.js
+++ app.js
@@ -1,1 +1,20 @@
-console.log("Hello, World!");
+const express = require("express");
+
+const app = express();
+const PORT = process.env.PORT || 8080;
+
+app.get("/", (req, res) => {
+  res.send("Hello, World!");
+});
+
+app.get("/ada", (req, res) => {
+  res.send("Hello, An Đức Anh!");
+});
+
+app.get("/cuocdoi", (req, res) => {
+  res.send("Hello, cuocdoi!");
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});
