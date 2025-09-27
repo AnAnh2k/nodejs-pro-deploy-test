84aad840a3f7e7dce72121b81e3287fc83fda964
AnAnh2k <anducanh125@gmail.com>
2025-09-27 14:57:50 +0700

    video 61

--- src/controllers/user.controller.ts
+++ src/controllers/user.controller.ts
@@ -21,7 +21,7 @@ const getCreateUserPage = (req: Request, res: Response) => {
 
 const postCreateUser = async (req: Request, res: Response) => {
   const { fullname, email, address } = req.body;
-  await handleCreateUser(fullname, email, address);
+  const a = await handleCreateUser(fullname, email, address);
 
   return res.redirect("/");
 };
