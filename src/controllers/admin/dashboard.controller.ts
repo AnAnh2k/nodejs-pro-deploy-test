f1e1b201c2c699889463be14ecaf8c18866421e3
AnAnh2k <anducanh125@gmail.com>
2025-09-29 19:21:54 +0700

    video 77

--- src/controllers/admin/dashboard.controller.ts
+++ src/controllers/admin/dashboard.controller.ts
@@ -1,13 +1,31 @@
 import e, { Request, Response } from "express";
+import { getAllUsers } from "services/user.service";
 
 const getDashboardPage = async (req: Request, res: Response) => {
   //get user
+
   return res.render("admin/dashboard/show");
 };
 
 const getAdminUserPage = async (req: Request, res: Response) => {
   //get user
-  return res.render("admin/user/show");
+  const users = await getAllUsers();
+  return res.render("admin/user/show", { users: users });
 };
 
-export { getDashboardPage, getAdminUserPage };
+const getAdminProductPage = async (req: Request, res: Response) => {
+  //get user
+  return res.render("admin/product/show");
+};
+
+const getAdminOrderPage = async (req: Request, res: Response) => {
+  //get user
+  return res.render("admin/order/show");
+};
+
+export {
+  getDashboardPage,
+  getAdminUserPage,
+  getAdminOrderPage,
+  getAdminProductPage,
+};
--- src/routes/web.ts
+++ src/routes/web.ts
@@ -13,6 +13,8 @@ import { get } from "http";
 import {
   getDashboardPage,
   getAdminUserPage,
+  getAdminProductPage,
+  getAdminOrderPage,
 } from "../controllers/admin/dashboard.controller";
 
 const webRoute = (app: Express) => {
@@ -23,11 +25,14 @@ const webRoute = (app: Express) => {
   router.get("/view-user/:id", getViewUser);
   router.post("/handle-update-user/:id", postUpdateUser);
   router.post("/handle-create-user", postCreateUser);
-  router.get("/create-user", getCreateUserPage);
 
   //admin page
   router.get("/admin", getDashboardPage);
   router.get("/admin/user", getAdminUserPage);
+  router.get("/admin/create-user", getCreateUserPage);
+
+  router.get("/admin/product", getAdminProductPage);
+  router.get("/admin/order", getAdminOrderPage);
 
   app.use("/", router);
 };
